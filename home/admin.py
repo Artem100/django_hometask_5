7e85856042787ab15973582b9b6b470c80d93472
artem.stolbtsov <artem>
2021-01-08 18:05:56 +0200

    Add student model

--- home/admin.py
+++ home/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Student
+
+class StudentsAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'surname', 'age', 'sex', 'address', 'description', 'birthday', 'email')
+    list_display_links = ('id', 'name', ) # Колонка становиться ссылкой
+    search_fields = ('name', 'surname') # Колонки по которым производится поиск
+    list_filter = ('name', 'surname') # Дополнительный фильтр
+
+admin.site.register(Student, StudentsAdmin)
--- home/models.py
+++ home/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+class Student(models.Model):
+    """
+    Student data of all students in school
+    """
+    #id = models.IntegerField(primary_key=True, verbose_name='Id')
+    name = models.CharField(max_length=200, verbose_name='Name')
+    surname = models.CharField(max_length=200, verbose_name='Surname')
+    age = models.IntegerField(blank=True, verbose_name='Age')
+    sex = models.TextField(blank=True, verbose_name='Sex')
+    address = models.TextField(blank=True, verbose_name='Address')
+    description = models.TextField(blank=True, verbose_name='Description')
+    birthday = models.DateField(blank=True, verbose_name="birthday")
+    email = models.EmailField(blank=True, verbose_name="Email")
+
+
+    def __str__(self):
+        return self.name
